eb7456779856dee492944321872af4f220b32c32
Rogerio <rserapa@gmail.com>
2020-03-18 20:48:33 -0300

    Criado a classe emprestimo.

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -5,6 +5,8 @@ public class BibliotecaApp {
 	public static void main(String[] args) {
 		
 		Livro l = new Livro();
+		Usuario u = new Usuario();
+		Emprestimo emprestimo = new Emprestimo();
 		
 	}
 
